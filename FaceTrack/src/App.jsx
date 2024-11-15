686a01465f17ca3c3db03df60e1adbebfe1640de
Tofu0mega <110778633+Tofu0mega@users.noreply.github.com>
2024-11-15 17:07:21 +0545

    Got the face model working

---
+++ FaceTrack/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState,useEffect } from 'react'
+
+import FaceTracking from './Videofeed'
+
+
+function App() {
+  
+
+
+  return (
+    <>
+   <FaceTracking/>
+    </>
+  )
+}
+
+export default App
